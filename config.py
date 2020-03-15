35ea396903d4b88abfcca2c16ef07731a4561398
Ebrahim Byagowi <ebrahim@gnu.org>
2020-03-15 22:11:20 +0330

    Make language and epochs configurable from shell

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import os
 import json
 
 import torch
@@ -7,7 +8,7 @@ gpu = torch.device('cuda')
 
 
 class DataConfig(object):
-    language = 'EN'
+    language = os.getenv('LANGUAGE', 'EN')
     graphemes_path = f'resources/{language}/Graphemes.json'
     phonemes_path = f'resources/{language}/Phonemes.json'
     lexicon_path = f'resources/{language}/Lexicon.json'
@@ -27,7 +28,7 @@ class TrainConfig(object):
     device = gpu if torch.cuda.is_available() else cpu
     lr = 3e-4
     batch_size = 128
-    epochs = 10
+    epochs = int(os.getenv('EPOCHS', '10'))
     log_path = f'log/{DataConfig.language}'
 
 
